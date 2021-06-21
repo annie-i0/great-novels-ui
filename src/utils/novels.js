6d0b7e033200e3e1a54b3b84871267eed56d6fb7
Annie <ikram.annie0@gmail.com>
2021-06-20 20:59:52 -0400

    created utils

--- src/utils/novels.js
+++ src/utils/novels.js
@@ -1,1 +1,15 @@
+import fetchNovels from "../actions/novels;
+
+export const filterNovels = (list, term) => {
+  return list.filter((novels) =>
+    novels.title.toLowerCase().includes(term.toLowerCase())
+  );
+};
+
+export const allNovels = async () => {
+
+  const novels = await fetchNovels();
+
+  return novels;
+};
 
